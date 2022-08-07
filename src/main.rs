57f87ff44fbfcd3a5ae59de272c1c4d30431207f
ERUIHNIYHBKBNF <839203521@qq.com>
2022-08-07 16:10:44 +0800

    args

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 mod cli;
 mod tasks;
 use structopt::StructOpt;
+use cli::{Action::*, CommandLineArgs};
+use tasks::*;
 
 fn main() {
-    let args = cli::CommandLineArgs::from_args();
-    println!("{:?}", args);
+    let args = CommandLineArgs::from_args();
+    
+    let journal_file = args.journal_file.expect("Filed to find journal file");
+
+    match args.action {
+        Add { task } => add_task(journal_file, Task::new(task)),
+        Done { position } => complete_task(journal_file, position),
+        List => list_tasks(journal_file),
+    }
+    .expect("Failed to complete action");
 }
