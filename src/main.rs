58bcf9730b8d2af0c9960238a63d0284803dce59
ERUIHNIYHBKBNF <839203521@qq.com>
2022-08-07 16:15:52 +0800

    default logs position

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,21 @@ mod tasks;
 use structopt::StructOpt;
 use cli::{Action::*, CommandLineArgs};
 use tasks::*;
+use std::path::PathBuf;
+
+fn find_default_journal_file() -> Option<PathBuf> {
+    home::home_dir().map(|mut path| {
+        path.push(".rusty-journal.json");
+        path
+    })
+}
 
 fn main() {
     let args = CommandLineArgs::from_args();
     
-    let journal_file = args.journal_file.expect("Filed to find journal file");
+    let journal_file = args.journal_file
+        .or_else(find_default_journal_file)
+        .expect("Filed to find journal file");
 
     match args.action {
         Add { task } => add_task(journal_file, Task::new(task)),
